24c514a0e5e20aa1c37008d9fd94f0e5380c5ccd
Kit Kong <kit@thinkingbox.com>
2022-08-07 13:18:54 -0700

    Upload completed project

---
+++ Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    private Color cursorColor = Color.black;
+    private int collisionCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hide default cursor and set circle to initial color
+        Cursor.visible = false;
+        gameObject.GetComponent<SpriteRenderer>().color = cursorColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If default cursor whenever it shows
+        if( Cursor.visible )
+        {
+            Cursor.visible = false;
+        }
+        // Lock circle to mouse position
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // Count number of rects overlapping for color mixing
+        collisionCount++;
+        ColorMixing(true, col.gameObject.GetComponent<SpriteRenderer>().color);
+
+        // Set circle color
+        gameObject.GetComponent<SpriteRenderer>().color = cursorColor;
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        // Count number of rects overlapping for color mixing
+        collisionCount--;
+        if( collisionCount <= 0)
+        {
+            cursorColor = Color.black;
+        }
+        else
+        {
+            ColorMixing(false, col.gameObject.GetComponent<SpriteRenderer>().color);
+        }
+        // Set circle color
+        gameObject.GetComponent<SpriteRenderer>().color = cursorColor;
+    }
+
+    private void ColorMixing(bool addition, Color otherCol)
+    {
+        // Calculates the new color
+        Color newCol = otherCol * 255;
+        Color oldCol = cursorColor * 255;
+        Vector4 result;
+        if( addition )
+        {
+            result = ((oldCol * oldCol) + (newCol * newCol)) / Mathf.Max(collisionCount - 1, 1);
+        }
+        else
+        {
+            result = ((oldCol * oldCol) - (newCol * newCol)) / Mathf.Max(collisionCount - 1, 1);
+        }
+
+        cursorColor = new Color(Mathf.Sqrt(result.x) / 255, Mathf.Sqrt(result.y) / 255, Mathf.Sqrt(result.z) / 255, 1.0f);
+    }
+}
---
+++ Assets/Scripts/Rects.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rects : MonoBehaviour
+{
+    public Color rectColor = Color.white;
+
+    private SpriteRenderer sRenderer;
+    private bool collision = false;
+    private float counter = 0.0f;
+    private float threshold = 3.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+        sRenderer.color = rectColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Start timer for circle hovering over rectangles
+        if( collision )
+        {
+            counter += Time.deltaTime;
+            if ( counter >= threshold )
+            {
+                collision = false;
+                counter = 0.0f;
+                SetColors();
+
+            }
+        }
+    }
+
+    private void SetColors()
+    {
+        // Finds all other rectangles of the same color through tags
+        GameObject[] others = GameObject.FindGameObjectsWithTag(gameObject.tag);
+        string newTag = "";
+
+        // Generates a new color that's different from the existing one
+        do
+        {
+            newTag = GenerateColor();
+
+        } while (newTag == gameObject.tag);
+
+        switch( newTag )
+        {
+            case "Red":
+                rectColor = Color.red;
+                break;
+            case "Green":
+                rectColor = Color.green;
+                break;
+            case "Blue":
+                rectColor = Color.blue;
+                break;
+            default:
+                rectColor = Color.blue;
+                break;
+        }
+
+        // If there are other rectangles of the same color, set their new color and tag
+        if( others.Length > 0 )
+        {
+            foreach( GameObject obj in others)
+            {
+                obj.tag = newTag;
+                obj.GetComponent<SpriteRenderer>().color = rectColor;
+            }
+        }
+        // Set rectangle with new color and tag
+        gameObject.tag = newTag;
+        sRenderer.color = rectColor;
+    }
+
+    private string GenerateColor()
+    {
+        string newTag = "";
+        switch (Random.Range(0, 2))
+        {
+            case 0:
+                newTag = "Red";
+                break;
+            case 1:
+                newTag = "Green";
+                break;
+            case 2:
+                newTag = "Blue";
+                break;
+            default:
+                newTag = "Blue";
+                break;
+        }
+
+        return newTag;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        collision = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        collision = false;
+        counter = 0.0f;
+    }
+}
